940b0d41e47cbd3135a78f410af3169155d4d727
otenyo <johnstone.omundi@student.moringaschool.com>
2021-06-30 21:52:33 +0300

    Added jQuery functions to js codes for restaurant page

---
+++ Restraunts/Restaurants.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    // $("#details1").hide();
+    $("#det1").click(function() {
+        $(".details1").toggle();
+    });
+    $("#location").click(function() {
+        $("#details1").toggle();
+    });
+    
+    $("#det2").click(function() {
+        $(".details2").toggle();
+    });
+    $("#location1").click(function() {
+        $("#details2").toggle();
+    });
+
+    $("#det3").click(function() {
+        $(".details3").toggle();
+    });
+    $("#location2").click(function() {
+        $("#details3").toggle();
+    });
+
+    $("#det4").click(function() {
+        $(".details4").toggle();
+    });
+    $("#location3").click(function() {
+        $("#details4").toggle();
+    });
+
+    $("#det5").click(function() {
+        $(".details5").toggle();
+    });
+    $("#location4").click(function() {
+        $("#details5").toggle();
+    });
+
+    $("#det6").click(function() {
+        $(".details6").toggle();
+    });
+    $("#location5").click(function() {
+        $("#details6").toggle();
+    });
+});
